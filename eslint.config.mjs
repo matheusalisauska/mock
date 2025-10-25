a1801487ce479296b1e96387c43ac70ab9d4c8f5
Matheus Alisauska <matheusalisauska@gmail.com>
2025-10-25 11:47:34 -0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,13 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+ {
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
